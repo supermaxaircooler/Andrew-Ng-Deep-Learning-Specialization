055bb798d883a24fe4410e4c5449bd9dbb95a572
supermaxaircooler <anishjangra3722@gmail.com>
2024-09-01 15:23:42 +0530

    first folder

---
+++ Neural Networks and Deep Learning/Week4/Building your Deep Neural Network - Step by Step/.ipynb_checkpoints/dnn_utils_v2-checkpoint.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+def sigmoid(Z):
+    """
+    Implements the sigmoid activation in numpy
+    
+    Arguments:
+    Z -- numpy array of any shape
+    
+    Returns:
+    A -- output of sigmoid(z), same shape as Z
+    cache -- returns Z as well, useful during backpropagation
+    """
+    
+    A = 1/(1+np.exp(-Z))
+    cache = Z
+    
+    return A, cache
+
+def relu(Z):
+    """
+    Implement the RELU function.
+
+    Arguments:
+    Z -- Output of the linear layer, of any shape
+
+    Returns:
+    A -- Post-activation parameter, of the same shape as Z
+    cache -- a python dictionary containing "A" ; stored for computing the backward pass efficiently
+    """
+    
+    A = np.maximum(0,Z)
+    
+    assert(A.shape == Z.shape)
+    
+    cache = Z 
+    return A, cache
+
+def relu_backward(da, cache):
+    z = cache
+    dz = np.array(da, copy=True)
+    dz[z <= 0] = 0
+    return dz
+
+def sigmoid_backward(da, cache):
+    z = cache
+    s = 1 / (1 + np.exp(-z))
+    dz = da * s * (1 - s)
+    return dz
+
